a297bc4588b3539b873a741825e921d2e9ca9393
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-10-05 12:48:11 -0700

    adding more starter code to modify

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const fileReader = require('./lib/reader.js');
+const callbacks = require('./lib/callbacks.js');
+const promises = require('./lib/promises.js');
+const async = require('./lib/async-await.js');
+
+let file = `${__dirname}/data/file.txt`;
+
+// A simple error first callback that regurgitates our file contents
+let showFileContents = (err,data) => {
+  if(err) { throw err; }
+  console.log(data);
+};
+
+// Use our custom file reader instead of normal fs, so that we can change our interface to it ...
+// Invoke our file reader.  Note that the "reader" module is expected to simply export a function, not an object
+// We should be able to call it directly with a file and any callback we like.
+
+fileReader(file, showFileContents);
+
+// Using our custom reader as a promise ...
+fileReader( file )
+  .then( contents => showFileContents(null, contents ) )
+  .catch( showFileContents );
+
